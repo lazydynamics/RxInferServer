0129048a2d4a89e0d103eed2de6943f10968b0a2
Bagaev Dmitry <bvdmitri@gmail.com>
2025-03-12 11:22:44 +0100

    check the ability to update the PR

--- src/tags/Authentification.jl
+++ src/tags/Authentification.jl
@@ -1,5 +1,5 @@
 
 
 function generate_token(req::HTTP.Request)::RxInferServerOpenAPI.TokenResponse
-    return RxInferServerOpenAPI.TokenResponse(token="1234567890")
+    return RxInferServerOpenAPI.TokenResponse(token="123456789")
 end

32e4e4be6d9fc0f04e9467565024785b477e77b0
Bagaev Dmitry <dbagaev@lazydynamics.com>
2025-03-15 17:58:42 +0100

    implement role based access to models (#24)

--- src/tags/Models.jl
+++ src/tags/Models.jl
@@ -3,8 +3,15 @@ function get_models(req::HTTP.Request)::HTTP.Response
     # Filter out private models
     models = Models.get_models()
 
+    # Filter models by role if provided
+    roles = current_roles()
+
+    filtered_models = filter(models) do model
+        return any(r -> r in roles, model.roles)
+    end
+
     # Create a list of lightweight model info
-    model_list = map(models) do model
+    model_list = map(filtered_models) do model
         return RxInferServerOpenAPI.LightweightModelInfo(name = model.name, description = model.description)
     end
 
@@ -15,6 +22,15 @@ function get_model_info(req::HTTP.Request, model_name::String)::HTTP.Response
     model = Models.get_model(model_name)
 
     if isnothing(model)
+        return HTTP.Response(
+            404,
+            RxInferServerOpenAPI.ErrorResponse(error = "Not Found", message = "The requested model could not be found")
+        )
+    end
+
+    roles = current_roles()
+
+    if !any(r -> r in roles, model.roles)
         return HTTP.Response(
             404,
             RxInferServerOpenAPI.ErrorResponse(error = "Not Found", message = "The requested model could not be found")
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -2,20 +2,107 @@
     using Test, RxInferServer
     using Base.ScopedValues
 
+    import Mongoc
     import RxInferClientOpenAPI
     import RxInferClientOpenAPI.OpenAPI.Clients: Client, set_header
-    import RxInferClientOpenAPI: ServerApi, AuthenticationApi
-
-    export TestClient
-    export ServerApi, AuthenticationApi
+    import RxInferClientOpenAPI: ServerApi, AuthenticationApi, ModelsApi
 
     const TEST_SERVER_URL = "http://localhost:8000$(RxInferServer.API_PATH_PREFIX)"
+    const TEST_TOKEN = ScopedValue{String}(RxInferServer.RXINFER_SERVER_DEV_TOKEN())
 
-    function TestClient(; authorized = true, token = RxInferServer.RXINFER_SERVER_DEV_TOKEN())
+    current_test_token() = TEST_TOKEN[]
+
+    function TestClient(; authorized = true, token = current_test_token())
         _client = Client(TEST_SERVER_URL)
-        if authorized
-            set_header(_client, "Authorization", "Bearer $(token)")
+        _token = @something token RxInferServer.RXINFER_SERVER_DEV_TOKEN()
+
+        if authorized && !isnothing(_token)
+            set_header(_client, "Authorization", "Bearer $(_token)")
         end
+
+        if authorized && isnothing(_token)
+            error(
+                "Cannot be authorized if no token is provided. Use `RXINFER_SERVER_DEV_TOKEN` environment variable to set a default token for testing purposes."
+            )
+        end
+
         return _client
+    end
+
+    function with_temporary_token(f::Function; role::String = "user")
+        _client = TestClient(authorized = false)
+
+        auth = RxInferClientOpenAPI.AuthenticationApi(_client)
+        response, info = RxInferClientOpenAPI.generate_token(auth)
+
+        if info.status != 200
+            error("Failed to generate a temporary token for role `$role`")
+        end
+
+        token = string(response.token)
+
+        # Update the token with the specified role in the database
+        RxInferServer.Database.with_connection() do
+            collection = RxInferServer.Database.collection("tokens")
+            query = Mongoc.BSON("token" => token)
+            update = Mongoc.BSON("\$set" => Mongoc.BSON("role" => role))
+            result = Mongoc.update_one(collection, query, update)
+
+            if result["modifiedCount"] != 1
+                error("Failed to update token with role `$role`")
+            end
+        end
+
+        try
+            with(TEST_TOKEN => token) do
+                f()
+            end
+        finally
+            # Delete the token from the database
+            RxInferServer.Database.with_connection() do
+                collection = RxInferServer.Database.collection("tokens")
+                query = Mongoc.BSON("token" => token)
+                result = Mongoc.delete_one(collection, query)
+
+                if result["deletedCount"] != 1
+                    error("Failed to delete token from the database")
+                end
+            end
+        end
+    end
+end
+
+@testitem "TestClient should be able to generate a token for a role" setup = [TestUtils] begin
+    client = TestUtils.TestClient()
+    @test client.headers["Authorization"] == "Bearer $(RxInferServer.RXINFER_SERVER_DEV_TOKEN())"
+end
+
+@testitem "TestClient should be able to generate a temporary token with an arbitrary role" setup = [TestUtils] begin
+    using Mongoc
+
+    created_token = Ref{String}("")
+    default_token = TestUtils.current_test_token()
+    TestUtils.with_temporary_token(role = "arbitrary,arbitrary2") do
+        client = TestUtils.TestClient()
+        temporary_token = TestUtils.current_test_token()
+        @test temporary_token != default_token
+        @test client.headers["Authorization"] == "Bearer $(temporary_token)"
+        created_token[] = temporary_token
+
+        # Check the role of the token in the database 
+        RxInferServer.Database.with_connection() do
+            collection = RxInferServer.Database.collection("tokens")
+            query = Mongoc.BSON("token" => created_token[])
+            result = Mongoc.find_one(collection, query)
+            @test result["role"] == "arbitrary,arbitrary2"
+        end
+    end
+
+    # Check that the token was deleted from the database
+    RxInferServer.Database.with_connection() do
+        collection = RxInferServer.Database.collection("tokens")
+        query = Mongoc.BSON("token" => created_token[])
+        result = Mongoc.find_one(collection, query)
+        @test isnothing(result)
     end
 end

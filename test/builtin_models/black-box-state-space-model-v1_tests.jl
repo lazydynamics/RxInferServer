47af44d46f898b697659962811da56a5925bf17b
Bagaev Dmitry <bvdmitri@gmail.com>
2025-03-28 15:04:47 +0100

    more tests

--- test/builtin_models/black-box-state-space-model-v1_tests.jl
+++ test/builtin_models/black-box-state-space-model-v1_tests.jl
@@ -39,9 +39,8 @@ end
 @testitem "model should be able to learn from observations" setup = [TestUtils] begin
     using RxInfer, StableRNGs
 
-    function generate_rotate_ssm_data(n; rng = StableRNG(1234))
-
-        θ = π / 8
+    function generate_rotate_ssm_data(n, k; rng = StableRNG(1234))
+        θ = π / k
         A = [cos(θ) -sin(θ); sin(θ) cos(θ)]
         Q = diageye(2)
         P = diageye(2)
@@ -53,57 +52,60 @@ end
 
         for i in 1:n
             x[i] = rand(rng, MvNormal(A * x_prev, Q))
-            y[i] = rand(rng, MvNormal(x[i], Q))
-
+            y[i] = rand(rng, MvNormal(x[i], P))
             x_prev = x[i]
         end
 
         return A, x, y
     end
 
-    A, x, y = generate_rotate_ssm_data(100)
+    @testset for k in (2, 4, 8)
+        A, x, y = generate_rotate_ssm_data(100, k)
 
-    client = TestUtils.TestClient()
-    models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+        client = TestUtils.TestClient()
+        models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
 
-    create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
-        model_name = "BlackBoxStateSpaceModel-v1",
-        description = "Testing black-box state space model",
-        arguments = Dict("state_dimension" => 2, "horizon" => 10)
-    )
+        create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+            model_name = "BlackBoxStateSpaceModel-v1",
+            description = "Testing black-box state space model",
+            arguments = Dict("state_dimension" => 2, "horizon" => 10)
+        )
 
-    response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
-
-    @test info.status == 200
-    @test !isnothing(response)
-
-    instance_id = response.instance_id
-
-    for i in eachindex(y)
-        inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(data = Dict("observation" => y[i]))
-        inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(models_api, instance_id, inference_request)
+        response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
 
         @test info.status == 200
-        @test !isnothing(inference_response)
+        @test !isnothing(response)
+
+        instance_id = response.instance_id
+
+        for i in eachindex(y)
+            inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(data = Dict("observation" => y[i]))
+            inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(
+                models_api, instance_id, inference_request
+            )
+
+            @test info.status == 200
+            @test !isnothing(inference_response)
+        end
+
+        # Double check that the episode has the correct number of events and that the events are correct
+        episode, info = TestUtils.RxInferClientOpenAPI.get_episode_info(models_api, instance_id, "default")
+        @test info.status == 200
+        @test !isnothing(episode)
+        @test length(episode.events) == length(y)
+        @test all(e -> e[1]["data"]["observation"] == e[2], zip(episode.events, y))
+
+        # Learn from the data
+        learn_request = TestUtils.RxInferClientOpenAPI.LearnRequest(episodes = ["default"])
+        learn_response, info = TestUtils.RxInferClientOpenAPI.run_learning(models_api, instance_id, learn_request)
+
+        @test info.status == 200
+        @test !isnothing(learn_response)
+        @test isapprox(A, reshape(learn_response.learned_parameters["A_flattened"], 2, 2), rtol = 0.1)
+
+        # Delete model instance
+        response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
+        @test info.status == 200
+        @test response.message == "Model instance deleted successfully"
     end
-
-    # Double check that the episode has the correct number of events and that the events are correct
-    episode, info = TestUtils.RxInferClientOpenAPI.get_episode_info(models_api, instance_id, "default")
-    @test info.status == 200
-    @test !isnothing(episode)
-    @test length(episode.events) == length(y)
-    @test all(e -> e[1]["data"]["observation"] == e[2], zip(episode.events, y))
-
-    # Learn from the data
-    learn_request = TestUtils.RxInferClientOpenAPI.LearnRequest(episodes = ["default"])
-    learn_response, info = TestUtils.RxInferClientOpenAPI.run_learning(models_api, instance_id, learn_request)
-
-    @test info.status == 200
-    @test !isnothing(learn_response)
-    @test isapprox(A, reshape(learn_response.learned_parameters["A_flattened"], 2, 2), rtol = 0.1)
-
-    # Delete model instance
-    response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
-    @test info.status == 200
-    @test response.message == "Model instance deleted successfully"
 end

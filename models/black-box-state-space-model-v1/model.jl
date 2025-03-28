1fa2d9fe9631f3181e76531714ef1c62e85da3a2
Bagaev Dmitry <bvdmitri@gmail.com>
2025-03-28 14:56:37 +0100

    zapihnul

--- models/black-box-state-space-model-v1/model.jl
+++ models/black-box-state-space-model-v1/model.jl
@@ -1,23 +1,68 @@
 using RxInfer
 
 function initial_state(arguments)
-    return Dict()
+    return Dict("state_dimension" => arguments["state_dimension"], "horizon" => arguments["horizon"])
 end
 
 function initial_parameters(arguments)
     return Dict("A" => zeros(arguments["state_dimension"], arguments["state_dimension"]))
 end
 
-@model function state_space_model_inference(A, horizon, current_state, observation, arguments)
-    dim = arguments["state_dimension"]::Int
-
-    s[0] ~ MvNormalMeanCovariance(μ = current_state, Σ = diageye(dim))
-    y[0] ~ MvNormalMeanCovariance(μ = s[0], Σ = diageye(dim))
+@model function state_space_model_prediction(dim, A, horizon, current_state, y)
+    s[0] ~ MvNormal(μ = current_state, Σ = diageye(dim))
+    y[0] ~ MvNormal(μ = s[0], Σ = diageye(dim))
 
     for t in 1:horizon
-        s[t] ~ MvNormalMeanCovariance(μ = A * s[t - 1], Σ = diageye(dim))
-        y[t] ~ MvNormalMeanCovariance(μ = s[t], Σ = diageye(dim))
+        s[t] ~ MvNormal(μ = A * s[t - 1], Σ = diageye(dim))
+        y[t] ~ MvNormal(μ = s[t], Σ = diageye(dim))
     end
+end
 
-    return y
+### ---------------------------------------------- ###
+### ------------------ LEARNING ------------------ ###
+### ---------------------------------------------- ###
+
+@meta function make_learning_meta(f)
+    ContinuousTransition() -> CTMeta(f)
+end
+
+@initialization function make_learning_initialization(dim)
+    q(H) = MvNormalMeanCovariance(zeros(dim * dim), 1e2 * diageye(dim * dim))
+end
+
+@constraints function make_learning_constraints()
+    q(H, s) = q(H)q(s)
+end
+
+@model function state_space_model_learning(dim, y)
+    s[1] ~ MvNormal(μ = ones(dim), Σ = diageye(dim))
+    y[1] ~ MvNormal(μ = s[1], Σ = diageye(dim))
+
+    H ~ MvNormal(μ = zeros(dim * dim), Σ = diageye(dim * dim))
+
+    for t in 2:length(y)
+        s[t] ~ ContinuousTransition(s[t - 1], H, diageye(dim))
+        y[t] ~ MvNormal(μ = s[t], Σ = diageye(dim))
+    end
+end
+
+function run_inference(state, parameters, data)
+    return Dict(), state
+end
+
+function run_learning(state, parameters, events)
+    f = let dim = state["state_dimension"]
+        (H) -> reshape(H, dim, dim)
+    end
+    y = [convert(Vector{Float64}, event["data"]["observation"]) for event in events]
+    results = infer(
+        model = state_space_model_learning(dim = state["state_dimension"]),
+        data = (y = y,),
+        meta = make_learning_meta(f),
+        initialization = make_learning_initialization(state["state_dimension"]),
+        constraints = make_learning_constraints(),
+        options = (limit_stack_depth = 100,)
+    )
+
+    return Dict(), state, parameters
 end
--- test/builtin_models/black-box-state-space-model-v1_tests.jl
+++ test/builtin_models/black-box-state-space-model-v1_tests.jl
@@ -35,3 +35,74 @@ end
     @test info.status == 200
     @test response.message == "Model instance deleted successfully"
 end
+
+@testitem "model should be able to learn from observations" setup = [TestUtils] begin
+    using RxInfer, StableRNGs
+
+    function generate_rotate_ssm_data(n; rng = StableRNG(1234))
+
+        θ = π / 8
+        A = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+        Q = diageye(2)
+        P = diageye(2)
+
+        x_prev = ones(2)
+
+        x = Vector{Vector{Float64}}(undef, n)
+        y = Vector{Vector{Float64}}(undef, n)
+
+        for i in 1:n
+            x[i] = rand(rng, MvNormal(A * x_prev, Q))
+            y[i] = rand(rng, MvNormal(x[i], Q))
+
+            x_prev = x[i]
+        end
+
+        return A, x, y
+    end
+
+    A, x, y = generate_rotate_ssm_data(100)
+
+    client = TestUtils.TestClient()
+    models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+
+    create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+        model_name = "BlackBoxStateSpaceModel-v1",
+        description = "Testing black-box state space model",
+        arguments = Dict("state_dimension" => 2, "horizon" => 10)
+    )
+
+    response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
+
+    @test info.status == 200
+    @test !isnothing(response)
+
+    instance_id = response.instance_id
+
+    for i in eachindex(y)
+        inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(data = Dict("observation" => y[i]))
+        inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(models_api, instance_id, inference_request)
+
+        @test info.status == 200
+        @test !isnothing(inference_response)
+    end
+
+    # Double check that the episode has the correct number of events and that the events are correct
+    episode, info = TestUtils.RxInferClientOpenAPI.get_episode_info(models_api, instance_id, "default")
+    @test info.status == 200
+    @test !isnothing(episode)
+    @test length(episode.events) == length(y)
+    @test all(e -> e[1]["data"]["observation"] == e[2], zip(episode.events, y))
+
+    # Learn from the data
+    learn_request = TestUtils.RxInferClientOpenAPI.LearnRequest(episodes = ["default"])
+    learn_response, info = TestUtils.RxInferClientOpenAPI.run_learning(models_api, instance_id, learn_request)
+
+    @test info.status == 200
+    @test !isnothing(learn_response)
+
+    # Delete model instance
+    response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
+    @test info.status == 200
+    @test response.message == "Model instance deleted successfully"
+end

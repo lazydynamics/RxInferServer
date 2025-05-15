7cd78dce1922c1dcc6881582e688bed7919434e5
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2025-05-15 19:44:59 +0200

    Add AR model (#72)
    * Add AR model
    
    * redo naming
    
    * make format
    
    * update iterations
    
    * better handling of optional arguments
    
    * rm state when learning
    
    * fix state updating

---
+++ models/autoregressive-v1/model.jl
@@ -0,0 +1,99 @@
+using RxInfer, LinearAlgebra
+
+function initial_state(arguments)
+    return Dict(
+        "order" => arguments["order"],
+        "horizon" => arguments["horizon"],
+        "x_μ" => get(arguments, "x_μ", zeros(arguments["order"])),
+        "x_Λ" => get(arguments, "x_Λ", diageye(arguments["order"]))
+    )
+end
+
+function initial_parameters(arguments)
+    return Dict(
+        "τ_α" => 1.0, "τ_β" => 1.0, "θ_μ" => zeros(arguments["order"]), "θ_Λ" => 1e-10*diageye(arguments["order"])
+    )
+end
+
+@model function AR_model(y, order, parameters, state)
+    # `c` is a unit vector of size `order` with first element equal to 1
+    c = ReactiveMP.ar_unit(Multivariate, order)
+
+    τ ~ Gamma(α = parameters["τ_α"], β = parameters["τ_β"])
+    θ ~ MvNormal(mean = parameters["θ_μ"], precision = parameters["θ_Λ"])
+    x0 ~ MvNormal(mean = state["x_μ"], precision = state["x_Λ"])
+
+    x_prev = x0
+
+    for i in eachindex(y)
+        x[i] ~ AR(x_prev, θ, τ)
+        y[i] ~ Normal(mean = dot(c, x[i]), precision = 1e10)
+
+        x_prev = x[i]
+    end
+end
+
+@constraints function AR_constraints()
+    q(x0, x, θ, τ, y) = q(x0, x)q(y)q(θ)q(τ)
+end
+
+@meta function AR_meta(order)
+    AR() -> ARMeta(Multivariate, order, ARsafe())
+end
+
+@initialization function AR_init(parameters)
+    q(τ) = GammaShapeRate(parameters["τ_α"], parameters["τ_β"])
+    q(θ) = MvNormalMeanPrecision(parameters["θ_μ"], parameters["θ_Λ"])
+end
+
+### ---------------------------------------------- ###
+### ----------------- INFERENCE ------------------ ###
+### ---------------------------------------------- ###
+function run_inference(state, parameters, data)
+
+    # Add missing values to the observations to match the horizon
+    converted_observations = convert.(Float64, data["observation"])
+    observations = vcat(converted_observations, [missing for _ in 1:state["horizon"]])
+
+    inference_results = infer(
+        model = AR_model(order = state["order"], parameters = parameters, state = state),
+        data = (y = UnfactorizedData(observations),),
+        meta = AR_meta(state["order"]),
+        constraints = AR_constraints(),
+        initialization = AR_init(parameters),
+        options = (limit_stack_depth = 300,),
+        iterations = 20,
+        returnvars = KeepLast()
+    )
+
+    result = Dict("states" => inference_results.posteriors[:x])
+    state["x_μ"] = mean(inference_results.posteriors[:x][length(converted_observations)])
+    state["x_Λ"] = precision(inference_results.posteriors[:x][length(converted_observations)])
+
+    return result, state
+end
+
+function run_learning(state, parameters, events)
+    observations = [convert(Float64, event["data"]["observation"]) for event in events]
+
+    inference_results = infer(
+        model = AR_model(order = state["order"], parameters = parameters, state = state),
+        data = (y = observations,),
+        meta = AR_meta(state["order"]),
+        constraints = AR_constraints(),
+        initialization = AR_init(parameters),
+        options = (limit_stack_depth = 300,),
+        returnvars = KeepLast(),
+        iterations = 20
+    )
+
+    # update parameters
+    parameters["τ_α"] = shape(inference_results.posteriors[:τ])
+    parameters["τ_β"] = rate(inference_results.posteriors[:τ])
+    parameters["θ_μ"] = mean(inference_results.posteriors[:θ])
+    parameters["θ_Λ"] = precision(inference_results.posteriors[:θ])
+
+    result = Dict("states" => state, "parameters" => parameters)
+
+    return result, state, parameters
+end
---
+++ test/models/autoregressive-v1_tests.jl
@@ -0,0 +1,178 @@
+@testitem "missing required arguments should lead to an error" setup = [TestUtils] begin
+    client = TestUtils.TestClient(roles = ["user"])
+    models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+
+    create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+        model_name = "Autoregressive-v1", description = "Testing autoregressive model"
+    )
+
+    response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
+
+    @test info.status == 400
+    @test !isnothing(response)
+    @test response.error == "Bad Request"
+    @test occursin("model configuration argument order is required", response.message)
+end
+
+@testitem "it should be possible to create a model with the correct arguments" setup = [TestUtils] begin
+    client = TestUtils.TestClient(roles = ["user"])
+    models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+
+    create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+        model_name = "Autoregressive-v1",
+        description = "Testing autoregressive model",
+        arguments = Dict("order" => 2, "horizon" => 5)
+    )
+
+    response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
+
+    @test info.status == 200
+    @test !isnothing(response)
+
+    instance_id = response.instance_id
+
+    response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
+    @test info.status == 200
+    @test response.message == "Model instance deleted successfully"
+end
+
+@testitem "model should be able to learn from observations and predict future signal" setup = [TestUtils] begin
+    using RxInfer, StableRNGs, LinearAlgebra
+
+    function generate_ar_data(n, order, horizon; rng = StableRNG(1234))
+        # AR coefficients
+        θ_true = [0.8, -0.5, 0.3][1:min(3, order)]
+        if order > 3
+            append!(θ_true, zeros(order - 3))
+        end
+
+        # Precision parameters
+        τ_true = 10.0  # Process noise precision
+        γ_true = 5.0   # Observation noise precision
+
+        # Generate data
+        x = zeros(n + horizon, order)
+        y = zeros(n + horizon)
+
+        # Initial states
+        for i in 1:order
+            x[i, :] = zeros(order)
+            x[i, 1] = rand(rng, Normal(0, 1/sqrt(τ_true)))
+            y[i] = x[i, 1] + rand(rng, Normal(0, 1/sqrt(γ_true)))
+        end
+
+        # Generate remaining data
+        for i in (order + 1):(n + horizon)
+            # Compute AR prediction
+            x_new = zeros(order)
+            x_new[1] = sum(θ_true .* x[i - 1, 1:order])
+
+            # Add process noise
+            x_new[1] += rand(rng, Normal(0, 1/sqrt(τ_true)))
+
+            # Shift states
+            x_new[2:end] = x[i - 1, 1:(order - 1)]
+
+            x[i, :] = x_new
+
+            # Generate observation with noise
+            y[i] = x[i, 1]
+        end
+
+        return θ_true, τ_true, x, y
+    end
+
+    @testset for n in (100, 200), order in (2, 3), horizon in (3, 5)
+        θ_true, τ_true, x_generated, y_generated = generate_ar_data(n, order, horizon)
+
+        y_train = y_generated[1:n]
+        y_future = y_generated[(n + 1):(n + horizon)]
+
+        client = TestUtils.TestClient(
+            roles = ["user"],
+            headers = Dict(
+                "Prefer" => "distributions_repr=data,distributions_data=mean_cov,mdarray_data=array_of_arrays,mdarray_repr=data"
+            )
+        )
+        models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+
+        create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+            model_name = "Autoregressive-v1",
+            description = "Testing autoregressive model",
+            arguments = Dict("order" => order, "horizon" => horizon)
+        )
+
+        response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
+
+        @test info.status == 200
+        @test !isnothing(response)
+
+        instance_id = response.instance_id
+
+        # Create events for all observations
+        events = [Dict("data" => Dict("observation" => obs)) for obs in y_train]
+
+        # Attach all events at once
+        attach_events_request = TestUtils.RxInferClientOpenAPI.AttachEventsToEpisodeRequest(events = events)
+        attach_response, info = TestUtils.RxInferClientOpenAPI.attach_events_to_episode(
+            models_api, instance_id, "default", attach_events_request
+        )
+
+        @test info.status == 200
+        @test !isnothing(attach_response)
+
+        # Double check that the episode has the correct number of events
+        episode, info = TestUtils.RxInferClientOpenAPI.get_episode_info(models_api, instance_id, "default")
+        @test info.status == 200
+        @test !isnothing(episode)
+        @test length(episode.events) == length(y_train)
+
+        # Learn from the data
+        learn_request = TestUtils.RxInferClientOpenAPI.LearnRequest(episodes = ["default"])
+        learn_response, info = TestUtils.RxInferClientOpenAPI.run_learning(models_api, instance_id, learn_request)
+
+        @test info.status == 200
+        @test !isnothing(learn_response)
+
+        # The learned θ parameter should be close to the true θ
+        @test haskey(learn_response.learned_parameters["parameters"], "θ_μ")
+        learned_θ = learn_response.learned_parameters["parameters"]["θ_μ"]
+        @test isapprox(learned_θ, θ_true, rtol = 0.2)
+
+        # Run inference on the latest data to get predictions
+        inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(data = Dict("observation" => [y_train[end]]))
+        inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(
+            models_api, instance_id, inference_request
+        )
+
+        @test info.status == 200
+        @test !isnothing(inference_response)
+
+        # Check that the predicted states are available
+        @test haskey(inference_response.results, "states")
+        predicted_states = inference_response.results["states"]
+        @test length(predicted_states) >= horizon
+
+        # The first state should correspond to the last observation
+        # Subsequent states should be predictions
+        for i in 1:horizon
+            # For each prediction, extract the relevant state value
+            predicted_mean = predicted_states[i + 1]["mean"][1]  # First component corresponds to observation
+
+            # Check that predictions are reasonable
+            @test isfinite(predicted_mean)
+
+            # For more strict testing, calculate prediction error
+            prediction_error = abs(predicted_mean - y_future[i])
+
+            # The error should be within a reasonable range
+            # This is a bit lenient due to the stochastic nature of the process
+            @test prediction_error < 3.0 * sqrt(1/τ_true)
+        end
+
+        # Delete model instance
+        response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
+        @test info.status == 200
+        @test response.message == "Model instance deleted successfully"
+    end
+end

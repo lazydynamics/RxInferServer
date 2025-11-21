1a8b113467d3d107b725d96eb658fbbc662b44e8
Wouter Nuijten <wouternuijten@gmail.com>
2025-11-21 15:35:25 +0100

    Add additional sanity test

--- test/models/hierarchical-binomial-v1_tests.jl
+++ test/models/hierarchical-binomial-v1_tests.jl
@@ -103,6 +103,51 @@ end
     @test response.message == "Model instance deleted successfully"
 end
 
+@testitem "model should be able to learn from observations" setup = [TestUtils] begin
+    using RxInfer, StableRNGs, LinearAlgebra
+
+    y_true = [[5, 5, 5, 5, 5], [10, 10, 10, 10, 10]]
+    n_trials = [[6, 6, 6, 6, 6], [10, 10, 10, 10, 10]]
+
+    client = TestUtils.TestClient(
+        roles = ["user"],
+        headers = Dict(
+            "Prefer" => "distributions_repr=data,distributions_data=mean_cov,mdarray_data=array_of_arrays,mdarray_repr=data"
+        )
+    )
+    models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
+
+    create_model_instance_request = TestUtils.RxInferClientOpenAPI.CreateModelInstanceRequest(
+        model_name = "HierarchicalBinomial-v1",
+        description = "Testing hierarchical binomial model",
+        arguments = Dict("n_components" => 5, "number_of_iterations" => 10)
+    )
+
+    response, info = TestUtils.RxInferClientOpenAPI.create_model_instance(models_api, create_model_instance_request)
+
+    @test info.status == 200
+    @test !isnothing(response)
+
+    instance_id = response.instance_id
+
+    # Create events for all observations
+    events = [Dict("data" => Dict("y" => y_true[i], "n_trials" => n_trials[i])) for i in 1:length(y_true)]
+    # Attach all events at once
+    attach_events_request = TestUtils.RxInferClientOpenAPI.AttachEventsToEpisodeRequest(events = events)
+    attach_response, info = TestUtils.RxInferClientOpenAPI.attach_events_to_episode(
+        models_api, instance_id, "default", attach_events_request
+    )
+    episode, info = TestUtils.RxInferClientOpenAPI.get_episode_info(models_api, instance_id, "default")
+    learn_request = TestUtils.RxInferClientOpenAPI.LearnRequest(episodes = ["default"])
+    learn_response, info = TestUtils.RxInferClientOpenAPI.run_learning(models_api, instance_id, learn_request)
+
+    # Test that the learned parameters are positive, trend is positive
+    @test learn_response.learned_parameters["latent_mean"] > 0.0
+
+    # Delete model instance
+    response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
+end
+
 @testitem "model should be able to infer given parameters" setup = [TestUtils] begin
     client = TestUtils.TestClient(roles = ["user"])
     models_api = TestUtils.RxInferClientOpenAPI.ModelsApi(client)
@@ -122,18 +167,14 @@ end
 
     test_n_trials = fill(10, 5)
 
-    inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(
-        data = Dict("n_trials" => test_n_trials)
-    )
-    inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(
-        models_api, instance_id, inference_request
-    )
+    inference_request = TestUtils.RxInferClientOpenAPI.InferRequest(data = Dict("n_trials" => test_n_trials))
+    inference_response, info = TestUtils.RxInferClientOpenAPI.run_inference(models_api, instance_id, inference_request)
 
     @test info.status == 200
     @test !isnothing(inference_response)
     @test haskey(inference_response.results, "y")
 
-    # TODO: Verify the predicted `y` values once the inference routine is implemented.
+    @test all(inference_response.results["y"] .≈ 5)
 
     response, info = TestUtils.RxInferClientOpenAPI.delete_model_instance(models_api, instance_id)
     @test info.status == 200
